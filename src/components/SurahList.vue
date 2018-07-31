37b6d8fba145295068cfaa1b7d08302423766933
Naveed Ahmad <naveedahmada036@gmail.com>
2018-07-31 20:07:27 +0500

    added fonts, and draft db

---
+++ src/components/SurahList.vue
@@ -0,0 +1,21 @@
+<template>
+  <f7-list medial-list>
+    <ul>
+      <SurahItem :surah="surah" v-for="surah in surahs"/>
+    </ul>
+  </f7-list>
+</template>
+
+<script>
+  import SurahItem from '../components/SurahItem';
+
+  export default {
+    props: [
+      'surahs',
+    ],
+    components: {
+      SurahItem,
+    }
+  }
+</script>
+
